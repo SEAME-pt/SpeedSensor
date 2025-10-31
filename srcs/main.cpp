c5b7fd4677df51e02a592ab23f7a1c4f0acba15f
jose5556 <josefilmeneses@gmail.com>
2025-10-31 18:23:39 +0000

    read signals correctly and calculates RPM

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,14 +1,34 @@
-#include "../include/VelocityMonitor.hpp"
+#include <stdio.h>
+#include <pigpio.h>
+#include <iostream>
+
+volatile int pulses = 0;
+
+void pulse_callback(int gpio, int level, uint32_t tick) {
+    if(level == 0) // FALLING edge
+        pulses++;
+}
 
 int main() {
+    if (gpioInitialise() < 0) {
+        std::cerr << "ERROR! Failed inicialization of pigpio. Aborting...\n" << std::endl;
+        return (1);
+    }
 
-	SpeedSensor_I2C sensor(0x68);  // example I2C address
-	if (!sensor.init()) {
-		std::cerr << "Failed to initialize sensor\n";
-		return 1;
-	}
+    int PIN = 24; // GPI24 = pino físico 24
+    gpioSetMode(PIN, PI_INPUT);
+    gpioSetPullUpDown(PIN, PI_PUD_UP);
 
-	VelocityMonitor monitor(sensor);
-	monitor.run();
-	return 0;
+    gpioSetAlertFunc(PIN, pulse_callback);
+
+    const int pulses_per_revolution = 20;
+    while(1) {
+        pulses = 0;
+        gpioDelay(5000000); // 5 segundos
+        double rpm = (pulses / 5.0) * 60.0 / pulses_per_revolution;
+        printf("Pulsos=%d, RPM≈%.1f\n", pulses, rpm);
+    }
+
+    gpioTerminate();
+    return 0;
 }
